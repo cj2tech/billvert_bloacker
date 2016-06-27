53b569b9472e78e3e342e38c939a1d6df65d4618
cj2tech <cj2tech@hotmail.com>
2016-06-27 00:38:27 -0400

    Delete sh_oldvert.lua

--- sh_oldvert.lua
+++
@@ -1,7 +0,0 @@
- timer.Simple( 1, function()
-DarkRP.chatCommands["billboard"] = {}
-DarkRP.chatCommands["billboard"] = DarkRP.chatCommands["advert"]
-DarkRP.chatCommands["billboard"]["command"] = "billboard"
-DarkRP.chatCommands["advert"] = nil
-end)
-
