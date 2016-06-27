c65d1d2ab029a36bc793a50e9b2f34f5f9db9261
cj2tech <cj2tech@hotmail.com>
2016-06-27 00:38:16 -0400

    Add files via upload

---
+++ billvert_blocker.lua
@@ -0,0 +1,7 @@
+ timer.Simple( 1, function()
+DarkRP.chatCommands["billboard"] = {}
+DarkRP.chatCommands["billboard"] = DarkRP.chatCommands["advert"]
+DarkRP.chatCommands["billboard"]["command"] = "billboard"
+DarkRP.chatCommands["advert"] = nil
+end)
+
